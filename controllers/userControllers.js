f0cf29a715f53b56960af2efbd2370a67ed0f82b
gitshubham45 <shubham16290@gmail.com>
2024-04-04 01:07:57 +0530

    token format modified

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -46,7 +46,7 @@ const authUser = asyncHandler(async (req, res) => {
             _id: user._id,
             name: user.name,
             email: user.email,
-            token: generateToken(user._id)
+            token: `Bearer ${generateToken(user._id)}`,
         });
     } else {
         res.status(400).json({ error: "Invalid email or password" });
